a28303601d4ee01c35dc511e3c2146c574223db4
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-02-23 15:40:55 +0700

    travis-ci: DSFMLC building added

--- test.sh
+++ test.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+git clone -b 2.1 --depth=1 https://github.com/Jebbs/DSFMLC.git
 git clone --depth=1 https://github.com/EsotericSoftware/spine-runtimes
+
+cd DSFMLC
+cmake .
+make
+sudo make install
+cd -
+
 cd spine-runtimes/spine-c
 cmake .
 make
