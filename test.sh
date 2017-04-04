e62c70fc0547a78ec9bc941934ff4aa9cf7c0843
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-04-04 11:03:30 +0700

    git push -f origin HEADCI fix: virtual x11 server added

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@
 git clone -b 2.1 --depth=1 https://github.com/Jebbs/DSFMLC.git
 git clone --depth=1 https://github.com/EsotericSoftware/spine-runtimes
 
+Xvfb -shmem -screen 0 1280x1024x24
+
 cd DSFMLC
 cmake .
 make
